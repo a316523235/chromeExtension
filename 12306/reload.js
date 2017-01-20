4494cc83166432d150a4a2b750c5151608bf60f1
huangzhijian <huangzhijian@linggan.com>
2017-01-20 15:57:29 +0800

    12306 extend

---
+++ 12306/reload.js
@@ -0,0 +1,7 @@
+console.log("extend is run");
+location.href = "https://kyfw.12306.cn/otn/";
+$(function() {
+  //$("#slideBox, #slideRBox, #ljimg, #shkyimg").remove();
+  //$("#indexLeftBL li").eq(2).children().click();
+});
+console.log("extend is run end");
